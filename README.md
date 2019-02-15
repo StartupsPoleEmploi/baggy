357ded9e8f4722e5f73e99dbf2d26617bd27b288
Régis Behmo <regis@behmo.com>
2019-02-15 11:28:18 +0100

    Improve mail-sending script

--- README.md
+++ README.md
@@ -34,6 +34,8 @@ There should be a web UI accessible at http://localhost. To daemonize, run inste
 
 For more information, check out the [Postal documentation](https://github.com/atech/postal/wiki).
 
+The `send.py` script can be run to test that the server is working properly.
+
 # License
 
 Baggy is licensed under the GNU Affero Public License (AGPL) v3. Some of the code in Baggy was borrowed from [docker-postal](https://github.com/CatDeployed/docker-postal/), which does not have an explicit license.
--- send.py
+++ send.py
@@ -1,21 +1,59 @@
 #! /usr/bin/env python3
+import argparse
 import requests
 
 def main():
+    parser = argparse.ArgumentParser(description="Send a test message using the Postal HTTP API")
+    parser.add_argument(
+        "-t", "--to",
+        nargs="+", required=True,
+        help="Email recipient. This option can be repeated multiple times"
+    )
+    parser.add_argument(
+        "-f", "--from",
+        dest="sender", required=True,
+        help="Email sender"
+    )
+    parser.add_argument(
+        "-s", "--subject",
+        default="Hello world",
+        help="Email subject"
+    )
+    parser.add_argument(
+        "-b", "--body",
+        default="",
+        help="Email body"
+    )
+    parser.add_argument(
+        "-k", "--key",
+        required=True,
+        help="API key"
+    )
+    parser.add_argument(
+        "-H", "--host",
+        default="localhost",
+        help="Postal host"
+    )
+    parser.add_argument(
+        "-p", "--port",
+        type=int, default=80,
+        help="Postal port"
+    )
+    args = parser.parse_args()
     response = requests.post(
-        "http://localhost/api/v1/send/message",
+        "http://{}:{}/api/v1/send/message".format(args.host, args.port),
         json={
-            "to": ["regis@behmo.com"],
-            "from": "pouac@jepostule.labonneboite.pole-emploi.fr",
-            "subject": "Bonjour monde !",
-            "plain_body": "怎么样？",
+            "to": args.to,
+            "from": args.sender,
+            "subject": args.subject,
+            "plain_body": args.body,
         },
         headers={
-            "X-Server-API-Key": "RqnjlOCr4ZNVzjar92bwLg2t",
+            "X-Server-API-Key": args.key,
         }
     )
-    print(response.status_code)
-    print(response.content)
+    print("Response code:", response.status_code)
+    print("Response body:", response.content.decode())
 
 if __name__ == "__main__":
     main()
